7408759a26206bf2d65646704e1f0de6304f4771
tunnelWithAC <conalldalydev@gmail.com>
2024-12-17 19:49:32 +0000

    Working greeter service

--- dotnet/grpc/field_masking/Client/Program.cs
+++ dotnet/grpc/field_masking/Client/Program.cs
@@ -16,14 +16,14 @@
 
 #endregion
 
-// using Greet;
+using Greet;
 using Grpc.Net.Client;
 
 using var channel = GrpcChannel.ForAddress("https://localhost:5001");
-// var client = new Greeter.GreeterClient(channel);
+var client = new Greeter.GreeterClient(channel);
 
-// var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
-// Console.WriteLine("Greeting: " + reply.Message);
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greeting: " + reply.Message);
 
 Console.WriteLine("Shutting down");
 // Console.WriteLine("Press any key to exit...");
--- dotnet/grpc/field_masking/README.md
+++ dotnet/grpc/field_masking/README.md
@@ -1,1 +1,9 @@
 Based on [gRPC API Performance Improvement Through Protobuf FieldMask In .NET](https://www.azilen.com/blog/grpc-api-performance-improvement-through-protobuf-fieldmask-in-net/)
+
+## Troubleshooting
+
+You can use the command below to fix `Unhandled exception. System.Net.Http.HttpRequestException: The SSL connection could not be established, see inner exception` when running locally.
+
+```
+dotnet dev-certs https --trust
+```
